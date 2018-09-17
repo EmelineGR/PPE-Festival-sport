ced0dd7b582adeffc6a1b42639874b95a89c148f
HUGON Alexandre <ahugon>
2018-09-17 14:35:44 +0200

    modif debut

--- _debut.inc.php
+++ _debut.inc.php
@@ -1,5 +1,4 @@
-<?
-
+<?php
 echo '
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
 "http://www.w3.org/TR/html4/loose.dtd">
@@ -33,6 +32,6 @@ echo '
       Attributions chambres</a></td>
    </tr>
 </table>
-<br>';
-
+<br>'
+?>
 
