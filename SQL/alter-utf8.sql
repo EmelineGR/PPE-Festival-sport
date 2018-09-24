918a961210d1e9cfec536fe3d7ae0302c6295d2f
Emeline <43139244+EmelineGR@users.noreply.github.com>
2018-09-24 17:03:27 +0200

    test accent

--- SQL/alter-utf8.sql
+++ SQL/alter-utf8.sql
@@ -2,7 +2,15 @@ Alter Table Attribution
 DROP FOREIGN KEY fk1_Attribution,
 DROP FOREIGN KEY fk2_Attribution;
 
+Alter Table Etablissement
+CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+Alter Table Groupe
+CHARACTER SET utf8 COLLATE utf8_general_ci;
+
 Alter Table Attribution
+CHARACTER SET utf8 COLLATE utf8_general_ci,
+
 ADD CONSTRAINT fk1_Attribution FOREIGN KEY (idEtab) 
 REFERENCES 	Etablissement(id),
 
